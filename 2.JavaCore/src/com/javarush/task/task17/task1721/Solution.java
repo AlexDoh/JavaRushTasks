e756986ba22e7abd4d04fe6594ebdd938e90c1c0
AlexDoh <odmitrenko1@gmail.com>
2017-06-15 17:56:37 +0300

    New Tasks

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +14,58 @@ import java.util.List;
 public class Solution {
     public static List<String> allLines = new ArrayList<String>();
     public static List<String> forRemoveLines = new ArrayList<String>();
+    public static final BufferedReader readerInput = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedReader readerFile;
+    public static String firstFile;
+    public static String secondFile;
 
-    public static void main(String[] args) {
+    static {
+        try {
+            firstFile = readerInput.readLine();
+            secondFile = readerInput.readLine();
+            readerFile = new BufferedReader(new FileReader(firstFile));
+            while (true) {
+                String line = readerFile.readLine();
+                if (line == null) {
+                    break;
+                }
+                allLines.add(line);
+            }
+            readerFile.close();
+            for (String s : allLines) {
+                System.out.println(s);
+            }
+            readerFile = new BufferedReader(new FileReader(secondFile));
+            String line;
+            while ((line = readerFile.readLine()) != null) {
+                forRemoveLines.add(line);
+            }
+            for (String s : forRemoveLines) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void joinData () throws CorruptedDataException {
+    public static void main(String[] args) {
+        Solution instance = new Solution();
+        try {
+            instance.joinData();
+            for (String s : allLines) {
+                System.out.println(s);
+            }
+        } catch (CorruptedDataException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void joinData() throws CorruptedDataException {
+        if (allLines.containsAll(forRemoveLines)) {
+            allLines.removeAll(forRemoveLines);
+        } else {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
     }
 }

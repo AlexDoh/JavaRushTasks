9804789b313a51f4ea7b57c6549374114b73693b
AlexDoh <odmitrenko1@gmail.com>
2017-06-21 14:38:04 +0300

    New Tasks

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -44,3 +44,57 @@ public class Solution {
         bufWrite.close();
     }
 }
+
+/*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        String line;
+        String idLine = "";
+        while (true) {
+            line = reader.readLine();
+            if (line == null) {
+                break;
+            } else {
+                idLine = line;
+            }
+        }
+        System.out.println(idLine);
+        Pattern pattern = Pattern.compile("^\\d+");
+        Matcher matcher = pattern.matcher(idLine);
+        matcher.find();
+
+        Integer id = Integer.parseInt(matcher.group(0));
+        id++;
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName, true);
+        fileOutputStream.write(id.toString().getBytes());
+
+        if (matcher.group(0).length() < 8) {
+            fileOutputStream.write(matcher.group(0).getBytes());
+            fileOutputStream.write(32);
+        } else {
+            fileOutputStream.write(matcher.group(0).getBytes());
+        }
+
+        if (args[1].length() < 30) {
+            fileOutputStream.write(args[1].getBytes());
+            fileOutputStream.write(32);
+        } else {
+            fileOutputStream.write(args[1].getBytes());
+        }
+
+        if (args[2].length() < 8) {
+            fileOutputStream.write(args[2].getBytes());
+            fileOutputStream.write(32);
+        } else {
+            fileOutputStream.write(args[2].getBytes());
+        }
+
+        if (args[3].length() < 4) {
+            fileOutputStream.write(args[3].getBytes());
+            fileOutputStream.write(32);
+        } else {
+            fileOutputStream.write(args[3].getBytes());
+        }*/

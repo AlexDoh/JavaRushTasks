426df55fc473ca23f06a8fcfeeeac77c8845c405
AlexDoh <odmitrenko1@gmail.com>
2017-07-05 18:40:14 +0300

    New Tasks

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2102/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2102/Solution.java
@@ -17,7 +17,7 @@ public class Solution {
     }
 
     public static boolean isAllModifiersContainSpecificModifier(int allModifiers, int specificModifier) {
-        return false;
+        return (allModifiers & specificModifier) == specificModifier;
     }
 
     private static Method getMainMethod() {

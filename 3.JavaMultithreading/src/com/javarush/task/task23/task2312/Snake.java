d61c4ed8e29cb50d437832eac52c8458d85d93ce
Alex <odmitrenko1@gmail.com>
2017-11-27 21:56:17 +0200

    New task.

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -57,4 +57,21 @@ public class Snake {
     public void move(int x, int y) {
 
     }
+
+    public void checkBorders(SnakeSection head) {
+        if (
+                head.getX() < 0 ||
+                head.getY() < 0 ||
+                head.getX() >= Room.game.getHeight() ||
+                head.getY() >= Room.game.getWidth()
+                ) {
+            isAlive = false;
+        }
+    }
+
+    public void checkBody(SnakeSection head) {
+        if(getSections().contains(head)) {
+            isAlive = false;
+        }
+    }
 }
